fb3755b3be980bff0fa164af635da3105930d89b
nmehanma <nmehanma@gmail.com>
2020-03-23 17:19:44 -0400

    remove tracking functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
