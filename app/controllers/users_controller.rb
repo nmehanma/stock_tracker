a15a35f03b6abf665146c47c5b4a980a0f2ccbbe
nmehanma <nmehanma@gmail.com>
2020-03-23 16:57:59 -0400

    display search results in the browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,16 @@ class UsersController < ApplicationController
 
   def search
     if params[:friend].present?
-      @friend = params[:friend]
-      if @friend
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
         respond_to do |format|
           format.js {render partial: 'users/friend_result'}
         end 
       else 
       respond_to do |format|
         flash.now[:alert]= "Couldn't find user"
-          format.js {render partial: 'users/_result' }
+          format.js {render partial: 'users/friend_result' }
         end
       end
     else
