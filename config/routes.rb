a46930106805d2889cc0920bad24b4abf999dd64
nmehanma <nmehanma@gmail.com>
2020-03-23 18:29:47 -0400

    display user friend/profile and tracking options

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
   resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
